53ea3bb150dc94bc4da4a1c1b06dd62c7c593ad8
Gabe Mertz <gmertz@lclark.edu>
2018-02-28 16:16:08 -0800

    added lab5, changed some 's to "s

--- lab4/fifteen.js
+++ lab4/fifteen.js
@@ -6,13 +6,13 @@ window.onload = function(){
     document.getElementById("shufflebutton").addEventListener("click",shuffle);
     for(var x = 0; x < 4; x++){
         for(var y = 0; y < 4; y++){
-            var blok = document.createElement('div');
-            var number = (y*4)+x + 1;
+            var blok = document.createElement("div");
+            var number = (y*4) + x + 1;
             if(number === 16){
-                blok.classList.add('blank');
+                blok.classList.add("blank");
                 blok.id = String(x)+String(y);
             }else{
-                blok.className = 'pieces';
+                blok.className = "pieces";
                 blok.innerHTML = number;
                 blok.id = String(x)+String(y);
                 blok.addEventListener("mouseover",check);
@@ -20,8 +20,8 @@ window.onload = function(){
 
             }
             blok.style.backgroundPosition = String(-100*x) + "px" + " " + String(-100*y) + "px";
-            blok.style.top = 100*y + 'px';
-            blok.style.left = 100*x + 'px';
+            blok.style.top = 100*y + "px";
+            blok.style.left = 100*x + "px";
 
             puz.append(blok);
         }
@@ -30,10 +30,10 @@ window.onload = function(){
 };
 
 function moveTile(tile){
-    tile.style.top = (parseInt(blank.id[1])*100) + 'px';
-    tile.style.left = (parseInt(blank.id[0])*100)+ 'px';
-    blank.style.top= (parseInt(tile.id[1])*100) + 'px';
-    blank.style.left= (parseInt(tile.id[0])*100) + 'px';
+    tile.style.top = (parseInt(blank.id[1])*100) + "px";
+    tile.style.left = (parseInt(blank.id[0])*100)+ "px";
+    blank.style.top= (parseInt(tile.id[1])*100) + "px";
+    blank.style.left= (parseInt(tile.id[0])*100) + "px";
     var temp = new String(tile.id);
     tile.id = new String(blank.id);
     blank.id = temp;
@@ -67,6 +67,6 @@ function shuffle(){
         if(y+1 < 4){moves.push([x,y+1]);}
         if(y-1 >= 0){moves.push([x,y-1]);}
         var move = Math.floor(Math.random() * moves.length);
-        moveTile(document.getElementById(String(moves[move][0])+String(moves[move][1])));
+        moveTile(document.getElementById(String(moves[move][0]) + String(moves[move][1])));
     }
 }
